a9bca8893a240bd81aba93718339b9ebcc767f05
Muzaffer-Irmak-Yavuz <muzafferirmaky@gmail.com>
2021-12-15 14:38:06 +0300

    #1 correct issues

--- indicator.py
+++ indicator.py
@@ -1,6 +1,7 @@
-def calculate_sma(list):
+def calculate_sma(closeList : list):
     sum=0
-    for i in list:
-        sum+=i
+    for i in closeList:
+        sum+=float(i)
+        print(float(i))
 
     return int(sum/10)
